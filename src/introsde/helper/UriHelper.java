c9c5f1de64b6f67de19e95e11e4b127fe70d0742
sn1p3r46 <kingokongo46@gmail.com>
2016-02-09 02:15:17 +0100

    heroku

--- src/introsde/helper/UriHelper.java
+++ src/introsde/helper/UriHelper.java
@@ -2,7 +2,7 @@ package introsde.helper;
 
 public class UriHelper{
 
-	static final String storageServiceURL = "http://127.0.1.1:5700/sdelab/";
+	static final String storageServiceURL = "https://damp-spire-46715.herokuapp.com/sdelab/";
 	static final String businessLogicURL  = "http://127.0.1.1:5900/sdelab/";
 
 	public static String getStorageServicesURL() {
